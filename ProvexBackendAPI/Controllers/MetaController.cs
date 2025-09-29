88ccb195cbdc7e5a943df06f2422a7e2c6927673
Hubert Ferrer <hferrer@provexsa.com>
2025-09-29 14:42:14 -0300

    feat: agregar MetaController con endpoints de versión y salud

---
+++ ProvexBackendAPI/Controllers/MetaController.cs
@@ -0,0 +1,36 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace ProvexBackendAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersionNeutral]
+    public class MetaController : ControllerBase
+    {
+        [HttpGet("version")]
+        [AllowAnonymous]
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+        public IActionResult Version()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var version = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                          ?? asm.GetName().Version?.ToString()
+                          ?? "unknown";
+            Response.Headers["X-App-Version"] = version;
+            return Ok(new
+            {
+                version,
+                serverTimeUtc = DateTime.UtcNow
+            });
+        }
+
+        [HttpGet("healthz")]
+        [AllowAnonymous]
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+        public IActionResult Healthz() => Ok(new { status = "ok" });
+    }
+}
