b4af48e159080623adf8c0a5b5c7792ef383bd8d
Hubert Ferrer <hferrer@provexsa.com>
2025-12-18 16:18:21 -0300

    Se adapta DistribucionController para devolver el SpResponse

--- ProvexBackendAPI/Controllers/DistribucionController.cs
+++ ProvexBackendAPI/Controllers/DistribucionController.cs
@@ -114,8 +114,8 @@ namespace ProvexBackendAPI.Controllers
             try
             {
                 var userId = await token.GetUserIdFromClaimsAsync(User);
-                await distribucion.DistribucionCalibreGuardarAsync(req, userId.Value);
-                return NoContent();
+                var result = await distribucion.DistribucionCalibreGuardarAsync(req, userId.Value);
+                return Ok(result);
             }
             catch (Exception e)
             {
