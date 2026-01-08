b1df7bbc5bc42e87ca97a2c9b5c936a05d0eaf6c
Matías Moncada <mmoncada@provexsa.com>
2026-01-08 11:36:14 -0300

    Avance programa comercial

---
+++ ProvexBackendAPI/Dto/AgrupacionEspecieCalibreResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ProvexBackendAPI.Dto
+{
+    public class AgrupacionEspecieCalibreResponse
+    {
+        public string idTemporada { get; set; }
+        public string idEmpresa { get; set; }
+        public string idEspecie { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ ProvexBackendAPI/Dto/RequestContextDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProvexBackendAPI.Dto
+{
+    public class RequestContextDTO
+    {
+        public string? idTemporada { get; set; }
+        public string? idEmpresa { get; set; }
+        public string? idEspecie { get; set; }
+    }
+}
